919eb2159a1b03b483cac744af8511d57da3b2c8
Michael King <michaelking.mgk@gmail.com>
2022-07-07 22:34:20 +0800

    Changed some notes.

--- hardhat-simple-storage-fcc/contracts/SimpleStorage.sol
+++ hardhat-simple-storage-fcc/contracts/SimpleStorage.sol
@@ -1,18 +1,17 @@
+/** The SimpleStorage contract is a simple smart contract which can store and retrieve data. **/
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8; // This sets the version of solidity you want to use, 0.8.7 is quite stable.
 
-/**
- *  This is our contracts file we compile it by running "yarn hardhat compile", all the compilation goes into the artifacts and chache files.
- *  To clean these files we run "yarn hardhat clean"
- */
+// This where we write our smart contract which will be deployed to a blockchain.
 contract SimpleStorage {
   // If we don't give a variable a number it sets it to a default value, for a uint256 it is 0.
   uint256 public favoriteNumber; // Adding the public keyword creates a getter function for the variable.
 
   // This mapping variable enables us to access the favoriteNumber associated to a name. Remember "public" keyword automatically adds a getter function.
+  // Initializes a mapping variable
   mapping(string => uint256) public nameToFavoriteNumber;
 
-  // This creates a People object
+  // This creates a "People" struct (similar to an object) with variables "favoriteNumber" and "name".
   struct People {
     uint256 favoriteNumber;
     string name;
@@ -29,19 +28,14 @@ contract SimpleStorage {
     nameToFavoriteNumber[_name] = _favoriteNumber;
   }
 
-  /**
-   *   The function store will change favoriteNumber variable to a new number.
-   *   _favoriteNumber function paramter (local variable indicated by _) that will be our new favoriteNumber (global variable) value.
-   *   Whenever we execute this function we are sending a transaction.
-   */
+  // This function (store) will change the global favoriteNumber variable to a new inputted number (_favoriteNumber), prefix "_" is used to indicate it is a local variable.
   function store(uint256 _favoriteNumber) public virtual {
-    //prefix it with _ to indicate variable is different from global variable.
     favoriteNumber = _favoriteNumber;
     //retrieve(); This will cost gas.
   }
 
   // View functions do not use gas since they do not modify the blockchain, unless they are called inside a function that uses gas.
-  function retrieve() public view returns (uint256) {c
+  function retrieve() public view returns (uint256) {
     return favoriteNumber;
   }
 }
