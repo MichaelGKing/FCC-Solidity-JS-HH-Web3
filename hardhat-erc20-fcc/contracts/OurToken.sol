a1e3398b898ece8594616fe2783977850a8a0c4f
Michael King <michaelking.mgk@gmail.com>
2022-07-08 19:00:39 +0800

    Changed token name.

--- hardhat-erc20-fcc/contracts/OurToken.sol
+++ hardhat-erc20-fcc/contracts/OurToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract OurToken is ERC20 {
     // If i say my initial supply is 50, that means 50 wei. Default the number of decimals is assumed at 18, but we can manually change this in the ERC20.sol module.
-    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+    constructor(uint256 initialSupply) ERC20("MikesToken", "MT") {
         _mint(msg.sender, initialSupply);
     }
 }
